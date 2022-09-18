ee9833553b1538ea2eb5d644a9f0056ed0fd6a8d
developerOlive <chloehiho@gmail.com>
2022-09-18 09:59:50 +0900

    [FEAT] first init

---
+++ src/main/java/com/fastcampus/spring_batch_fastcampus/SpringBatchFastcampusApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.spring_batch_fastcampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchFastcampusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchFastcampusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fastcampus/spring_batch_fastcampus/SpringBatchFastcampusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.spring_batch_fastcampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchFastcampusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
