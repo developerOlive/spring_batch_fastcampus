d2a2c166717c229e932e9050adc3f6056ec44990
developerOlive <chloehiho@gmail.com>
2022-09-18 11:42:15 +0900

    [FEAT] application.yml 파일 추가, 스프링 배치 실행을 위한 @EnableBatchProcessing 어노테이션 추가

--- src/main/java/com/fastcampus/spring_batch_fastcampus/SpringBatchFastcampusApplication.java
+++ src/main/java/com/fastcampus/spring_batch_fastcampus/SpringBatchFastcampusApplication.java
@@ -1,8 +1,10 @@
 package com.fastcampus.spring_batch_fastcampus;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableBatchProcessing
 @SpringBootApplication
 public class SpringBatchFastcampusApplication {
 
