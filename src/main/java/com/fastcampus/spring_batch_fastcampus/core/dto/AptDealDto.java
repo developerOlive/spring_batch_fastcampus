101c7c23c7d3a2745ecfce6c7e316f32e4d000ea
developerOlive <chloehiho@gmail.com>
2022-09-18 15:35:56 +0900

    [FEAT/실 거래가 수집 배치] API 응답결과를 저장할 AptDealDto 생성
    - 테스트를 위한 샘플데이터(apartment-api-response.txt) 추가

---
+++ src/main/java/com/fastcampus/spring_batch_fastcampus/core/dto/AptDealDto.java
@@ -0,0 +1,55 @@
+package com.fastcampus.spring_batch_fastcampus.core.dto;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/*
+ * 아파트 실거래가 API의 각각의 거래정보를 담는 객체
+ */
+@ToString
+@Getter
+@XmlRootElement(name = "item")
+public class AptDealDto {
+
+    @XmlElement(name = "거래금액")
+    private String dealAmount;
+
+    @XmlElement(name = "건축년도")
+    private Integer builtYear;
+
+    @XmlElement(name = "년")
+    private Integer year;
+
+    @XmlElement(name = "법정동")
+    private Integer dong;
+
+    @XmlElement(name = "아파트")
+    private Integer aptName;
+
+    @XmlElement(name = "월")
+    private Integer month;
+
+    @XmlElement(name = "일")
+    private Integer day;
+
+    @XmlElement(name = "전용면적")
+    private Double exclusiveArea;
+
+    @XmlElement(name = "지번")
+    private Integer jibun;
+
+    @XmlElement(name = "지역코드")
+    private Integer regionalCode;
+
+    @XmlElement(name = "층")
+    private Integer floor;
+
+    @XmlElement(name = "해제사유발생일")
+    private Integer dealCanceledDate;
+
+    @XmlElement(name = "해제여부")
+    private Integer dealCanceled;
+}
